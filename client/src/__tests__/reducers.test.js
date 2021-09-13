639899b6007fdb5429519294a84461cc05c26e40
EricaBabb <ecamilebabb@gmail.com>
2021-09-12 23:33:21 -0400

    productlist

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -23,3 +23,24 @@ import {
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
   });
+
+  test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CATEGORIES,
+      categories: [{}, {}]
+    });
+  
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+  });
+  //This indicates that we didn't affect our original state values at all and simply used it to create a new version of it.
+
+  test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: '2'
+    });
+  
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+  });
--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 
-function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+function CategoryMenu() {
+  //Old code:
+  // const categories = categoryData?.categories || [];
 
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            setCategory(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+  //retrieve the current state from the global state object and the dispatch() method to update state
+const [state, dispatch] = useStoreContext();
+//we only need the categories array out of our global state, so we destructure it out of state to provide to our returning JSX
+const { categories } = state;
+const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+//need categoryData that returns from QUERY_CAT. to use in dispatch so state can update. 
+//We can't just add the dispatch() method below it, b/c if Q_C is async then categoryData won't exist on load
+
+//useEffect was meant for this situation: useEffect is a function that takes two arguments, a function to run given a certain condition, and then the condition.
+
+useEffect(() => {
+  // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+  if (categoryData) {
+    // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+    dispatch({
+      type: UPDATE_CATEGORIES,
+      categories: categoryData.categories
+    });
+  }
+  //the condition that categoryData exists (QUERY_CAT. finished to present cat.data)
+  //useEffect renders continuously, so even if QUERY_CAT isn't done, meaning this condidition isnt fulfilled to run the inside function, it will run eventually. 
+}, [categoryData, dispatch]);
+
+//click-handler to update global state
+const handleClick = id => {
+  dispatch({
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: id
+  });
+};
+
+return (
+  <div>
+    <h2>Choose a Category:</h2>
+    {/* map over every item in the categories array */}
+    {categories.map(item => (
+      <button
+      // each item has a specific id
+        key={item._id}
+        // when this item is clicked the handleClick function will run––adds category id to the global state
+        onClick={() => {
+          handleClick(item._id);
+        }}
+      >
+        {item.name}
+      </button>
+    ))}
+  </div>
+);
+
 }
 
 export default CategoryMenu;
